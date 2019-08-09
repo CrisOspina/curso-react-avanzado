38a7000e9b498cd934ddb3083881e62ebb1967e8
Cristian Ospina <crisospina523@gmail.com>
2019-08-09 18:46:48 -0500

    new config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,9 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
+            plugins: [
+              '@babel/plugin-syntax-dynamic-import'
+            ],
             presets: [
               '@babel/preset-env',
               '@babel/preset-react'
