69eb1900117fb674a7af2070fbfe4a378d98ac0b
Cristian Ospina <crisospina523@gmail.com>
2019-08-09 18:54:06 -0500

    División de componente usando hooks reutilizables

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,41 +1,29 @@
-import React, { useState, useEffect, Fragment } from 'react'
+import React, { Fragment } from 'react'
+import { useCategoriesData } from '../../hooks/useCategoriesData'
+import { useShowOrFixedData } from '../../hooks/useShowOrFixedData'
 import { List, Item } from './styles'
+import { Loader } from '../../styles/loader'
 import { Category } from '../Category'
 
 export const ListOfCategories = () => {
-  const [categories, setCategories] = useState([])
-  const [showFixed, setShowFixed] = useState(false)
-
-  useEffect(() => {
-    window.fetch(`https://petgram-api-cris.now.sh/categories`)
-      .then(res => res.json())
-      .then(response => {
-        setCategories(response)
-      })
-  }, [])
-
-  useEffect(() => {
-    const onScroll = e => {
-      const newShowFixed = window.scrollY > 200
-      showFixed !== newShowFixed && setShowFixed(newShowFixed)
-    }
-
-    document.addEventListener('scroll', onScroll)
-
-    return () => document.removeEventListener('scroll', onScroll)
-  }, [showFixed])
+  const { categories, loading } = useCategoriesData()
+  const { showFixed } = useShowOrFixedData()
 
   const renderList = (fixed) => (
-    <List className={fixed ? 'fixed' : ''}>
+    <List fixed={fixed}>
       {
-        categories.map(category => (
-          <Item key={category.id}>
-            <Category {...category} />
-          </Item>
-        ))
+        loading
+          ? <Item key='loading'><Category /></Item>
+          : categories.map(category => (
+            <Item key={category.id}>
+              <Category {...category} />
+            </Item>
+          ))
       }
     </List>
   )
+
+  if (loading) return <Loader size={60} />
 
   return (
     <Fragment>
