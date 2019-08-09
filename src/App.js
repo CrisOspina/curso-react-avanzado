4f6e52960aaa172ad2b8b126b8a0a52fe3379c8d
Cristian Ospina <crisospina523@gmail.com>
2019-08-09 18:51:30 -0500

    main containers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { ListOfCategories } from './components/ListOfCategories'
-import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { ListOfPhotoCards } from './container/ListOfPhotoCards'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 
-export const App = () => (
-  <>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards />
-  </>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+
+  return (
+    <>
+      <GlobalStyle />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <Fragment>
+            <ListOfCategories />
+            <ListOfPhotoCards
+              categoryId={1}
+            />
+          </Fragment>
+      }
+    </>
+  )
+}
