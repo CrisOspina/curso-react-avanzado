02af5bd89caddcdaffef1abc7995abe3f2d6ad3a
Cristian Ospina <crisospina523@gmail.com>
2019-08-09 18:58:51 -0500

    Agregando ApolloClient y ApolloProvider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
 import { App } from './App'
 
+const client = new ApolloClient({
+  uri: 'https://petgram-api-cris.now.sh/graphql'
+})
+
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('app')
 )
