5743232f698a0fee7d20d18604c3686a53dde2db
Cristian Ospina <crisospina523@gmail.com>
2019-08-09 19:12:59 -0500

    Custom hook - para dectetar el scroll en dirección Y

---
+++ src/hooks/useShowOrFixedData.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+
+export function useShowOrFixedData () {
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  return { showFixed }
+}
