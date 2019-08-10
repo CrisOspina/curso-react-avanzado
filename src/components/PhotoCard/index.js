1078d321d3f091ccd01d46357a15ff43039d3330
Cristian Ospina <crisospina523@gmail.com>
2019-08-09 19:01:15 -0500

    Dividiendo componente utilizando custom hooks reutilizables

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,23 +1,37 @@
-import React from 'react'
-import { ImgWrapper, Img, Button } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import React, { Fragment } from 'react'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
+import { ImgWrapper, Img, Button, Article } from './styles'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 
 const DEFAULT_IMAGE = `https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png`
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  return (
-    <article>
-      <a href={`/detail/${id}`} >
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
+  const { show, element } = useNearScreen()
 
-      <Button>
-        <MdFavoriteBorder
-          size='32px'
-        /> {likes} likes!
-      </Button>
-    </article>
+  const key = `like=${id}`
+
+  const [liked, setLiked] = useLocalStorage(key, false)
+
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
+
+  return (
+    <Article ref={element}>
+      {
+        show && <Fragment>
+          <a href={`/?detail=${id}`} >
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+
+          <Button onClick={() => setLiked(!liked)}>
+            <Icon
+              size='32px'
+            /> {likes} likes!
+          </Button>
+        </Fragment>
+      }
+    </Article>
   )
 }
