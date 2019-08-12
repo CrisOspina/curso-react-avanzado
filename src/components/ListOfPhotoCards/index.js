2851a411858f664606af920e1e16513e12f6db1f
Cristian Ospina <crisospina523@gmail.com>
2019-08-11 21:11:38 -0500

    update component

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
 
-export const ListOfPhotoCardsComponent = ({
-  data: { photos = []
-  }
-} = {}) => {
+export const ListOfPhotoCards = ({ photos }) => {
   return (
     <u>
       {
