cec3f8abf685c4396a7ecaeeb6bece3e78e9437d
Cristian Ospina <crisospina523@gmail.com>
2019-08-11 21:06:17 -0500

    add link reach router

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Anchor, Image } from './styles'
+import { Link, Image } from './styles'
 
 const DEFAULT_IMAGE = 'https://images.pexels.com/photos/2614/night-animal-pet-eyes.jpg?cs=srgb&dl=animal-black-cat-2614.jpg&fm=jpg'
 
-export const Category = ({ cover = DEFAULT_IMAGE, path, emoji = '?' }) => (
-  <Anchor href={path}>
+export const Category = ({ cover = DEFAULT_IMAGE, path = '#', emoji = '?' }) => (
+  <Link to={path}>
     <Image src={cover} />
     {emoji}
-  </Anchor>
+  </Link>
 )
