32c76ba7e11716034797f1d4b4dd968baba1d12c
Cristian Ospina <crisospina523@gmail.com>
2019-08-11 20:39:36 -0500

    config new rutas - validación isAuth

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+import { Router } from '@reach/router'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import { ListOfCategories } from './components/ListOfCategories'
-import { ListOfPhotoCards } from './container/ListOfPhotoCards'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
+import { NavBar } from './components/NavBar'
+import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
-
   return (
     <>
       <GlobalStyle />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : <Fragment>
-            <ListOfCategories />
-            <ListOfPhotoCards
-              categoryId={1}
-            />
-          </Fragment>
-      }
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:categoryId' />
+        <Detail path='/detail/:detailId' />
+      </Router>
+
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs/' />
+                <User path='/user/' />
+              </Router>
+              : <Router>
+                <NotRegisteredUser path='/favs/' />
+                <NotRegisteredUser path='/user/' />
+              </Router>
+        }
+      </UserLogged>
+      <NavBar />
     </>
   )
 }
