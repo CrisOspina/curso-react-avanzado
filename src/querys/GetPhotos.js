22cfc51a692e3000dea334b2b442aa84e96ea027
Cristian Ospina <crisospina523@gmail.com>
2019-08-11 21:12:33 -0500

    query - todas las fotos

---
+++ src/querys/GetPhotos.js
@@ -0,0 +1,14 @@
+import { gql } from 'apollo-boost'
+
+export const GET_PHOTOS = gql`
+  query getPhotos($categoryId: ID) {
+    photos(categoryId: $categoryId) {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`
