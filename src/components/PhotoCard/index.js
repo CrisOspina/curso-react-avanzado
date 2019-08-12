9909f6d6095d4ce457db4c227fbc357b782f5bd8
Cristian Ospina <crisospina523@gmail.com>
2019-08-11 21:11:04 -0500

    implementación de mutación para el like

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,35 +1,40 @@
 import React, { Fragment } from 'react'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
-import { ImgWrapper, Img, Button, Article } from './styles'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { ImgWrapper, Img, Article } from './styles'
+import { FavButton } from '../FavButton'
+import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
+import { Link } from '@reach/router'
 
 const DEFAULT_IMAGE = `https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png`
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const { show, element } = useNearScreen()
-
   const key = `like=${id}`
-
   const [liked, setLiked] = useLocalStorage(key, false)
-
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={element}>
       {
         show && <Fragment>
-          <a href={`/?detail=${id}`} >
+          <Link to={`/detail/${id}`} >
             <ImgWrapper>
               <Img src={src} />
             </ImgWrapper>
-          </a>
+          </Link>
 
-          <Button onClick={() => setLiked(!liked)}>
-            <Icon
-              size='32px'
-            /> {likes} likes!
-          </Button>
+          <ToggleLikeMutation>
+            {
+              (toggleLike) => {
+                const handleFavClick = () => {
+                  !liked && toggleLike({ variables: { input: { id } }
+                  })
+                  setLiked(!liked)
+                }
+                return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
+              }
+            }
+          </ToggleLikeMutation>
         </Fragment>
       }
     </Article>
