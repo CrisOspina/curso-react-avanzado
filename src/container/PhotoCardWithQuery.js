bdfce4d9037bbfa98ae1b6cd6e1ceb7d10bb6415
Cristian Ospina <crisospina523@gmail.com>
2019-08-11 21:08:59 -0500

    container render props

--- src/container/PhotoCardWithQuery.js
+++ src/container/PhotoCardWithQuery.js
@@ -1,28 +1,18 @@
 import React from 'react'
-import { gql } from 'apollo-boost'
 import { Query } from 'react-apollo'
 import { PhotoCard } from '../components/PhotoCard'
+import { GET_SINGLE_PHOTO } from '../querys/GetSinglePhoto'
+import { Loader } from '../styles/loader'
 
-const query = gql`
-  query getSinglePhoto($id: ID!){
-    photo(id: $id){
-      id
-      categoryId
-      src
-      likes
-      userId
-      liked
-    }
-  }
-`
+const renderProp = ({ loading, error, data }) => {
+  if (loading) return <Loader size={60} />
+  if (error) return <h1>Error verifica...</h1>
+  const { photo = {} } = data
+  return <PhotoCard {...photo} />
+}
 
 export const PhotoCardWithQuery = ({ id }) => (
-  <Query query={query} variables={{ id }}>
-    {
-      ({ loading, error, data }) => {
-        const { photo = {} } = data
-        return <PhotoCard {...photo} />
-      }
-    }
+  <Query query={GET_SINGLE_PHOTO} variables={{ id }}>
+    { renderProp }
   </Query>
 )
