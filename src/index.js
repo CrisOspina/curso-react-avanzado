c80dfe70211310e55361a154b312dfea11175f6e
Cristian Ospina <crisospina523@gmail.com>
2019-08-14 14:44:13 -0500

    add context and JWT

--- src/index.js
+++ src/index.js
@@ -3,15 +3,35 @@ import ReactDOM from 'react-dom'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 
+import Context from './Context'
+
 import { App } from './App'
 
 const client = new ApolloClient({
-  uri: 'https://petgram-api-cris.now.sh/graphql'
+  uri: 'https://petgram-api-cris.now.sh/graphql',
+  request: operation => {
+    const token = window.sessionStorage.getItem('token')
+    const authorization = token ? `Bearer ${token}` : ''
+    operation.setContext({
+      headers: {
+        authorization
+      }
+    })
+  },
+  onError: error => {
+    const { networkError } = error
+    if (networkError && networkError.result.code === 'invalid_token') {
+      window.sessionStorage.removeItem('token')
+      window.location.href = '/'
+    }
+  }
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <Context.Provider>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Context.Provider>,
   document.getElementById('app')
 )
