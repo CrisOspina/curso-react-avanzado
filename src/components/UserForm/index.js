39cffa188c8be002fa5a4d04ff2e21b2b552aca3
Cristian Ospina <crisospina523@gmail.com>
2019-08-14 14:46:25 -0500

    add form registro-login

---
+++ src/components/UserForm/index.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from 'react'
+import { useInputValue } from '../../hooks/useInputValue'
+import { Form, Input, Button, Title, Error } from './styles'
+
+export const UserForm = ({ onSubmit, title, error, disabled }) => {
+  const email = useInputValue('')
+  const password = useInputValue('')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    onSubmit({ email: email.value, password: password.value })
+  }
+
+  return (
+    <Fragment>
+      <Form onSubmit={handleSubmit} disabled={disabled}>
+        <Title>{title}</Title>
+        <Input
+          placeholder='Email' {...email} disabled={disabled} />
+
+        <Input
+          placeholder='Password' type='password' {...password} disabled={disabled} />
+
+        <Button>{title}</Button>
+      </Form>
+      {error && <Error>{error}</Error>}
+    </Fragment>
+  )
+}
