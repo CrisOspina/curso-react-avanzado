d28556267bf59d476a89045565759cd74cf701fa
Cristian Ospina <crisospina523@gmail.com>
2019-08-14 14:47:45 -0500

    config registro y login

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { Fragment, useContext } from 'react'
+import { Context } from '../Context'
+import { UserForm } from '../components/UserForm'
+import { RegisterMutation } from '../containers/RegisterMutation'
+import { LoginMutation } from '../containers/LoginMutation'
 
 export const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context)
   return (
-    <h1>Not Registered User</h1>
+    <Fragment>
+      <RegisterMutation>
+        {
+          (register, { data, loading, error }) => {
+            const onSubmit = ({ email, password }) => {
+              const input = { email, password }
+              const variables = { input }
+              register({ variables })
+                .then(({ data }) => {
+                  const { signup } = data
+                  activateAuth(signup)
+                })
+            }
+
+            const errorMsg = error && 'El usuario ya existe, verifica...'
+
+            return (
+              <UserForm
+                onSubmit={onSubmit}
+                title='Registrarse'
+                error={errorMsg}
+                disabled={loading} />
+            )
+          }
+        }
+      </RegisterMutation>
+
+      <LoginMutation>
+        {
+          (login, { data, loading, error }) => {
+            const onSubmit = ({ email, password }) => {
+              const input = { email, password }
+              const variables = { input }
+              login({ variables })
+                .then(({ data }) => {
+                  const { login } = data
+                  activateAuth(login)
+                })
+            }
+
+            const errorMsg = error && 'La contraseña no es correcta o el usuario no existe'
+
+            return (
+              <UserForm
+                onSubmit={onSubmit}
+                title='Iniciar sesión'
+                error={errorMsg}
+                disabled={loading} />
+            )
+          }
+        }
+      </LoginMutation>
+
+    </Fragment>
+
   )
 }
