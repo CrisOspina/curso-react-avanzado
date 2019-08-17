8f043b184d8d06f0d0800d4862b6ecd51c5410d4
Cristian Ospina <crisospina523@gmail.com>
2019-08-16 22:18:29 -0500

    add page 404

---
+++ src/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Layout } from '../components/Layout'
+
+export default () => {
+  return (
+    <Layout title='404 - :('>
+      <h1>Esta página no existe!</h1>
+    </Layout>
+  )
+}
