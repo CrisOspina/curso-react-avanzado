93e525d63e7c246e250d76afd4b84c4487667e77
Cristian Ospina <crisospina523@gmail.com>
2019-08-16 22:31:37 -0500

    update component photo card

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -4,6 +4,7 @@ import { ImgWrapper, Img, Article } from './styles'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../containers/ToggleLikeMutation'
 import { Link } from '@reach/router'
+import PropTypes from 'prop-types'
 
 const DEFAULT_IMAGE = `https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png`
 
@@ -35,4 +36,15 @@ export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
       }
     </Article>
   )
+}
+
+PhotoCard.propTypes = {
+  id: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  src: PropTypes.string.isRequired,
+  likes: (props, propName, componentName) => {
+    const propValue = props[propName]
+    if (propValue === undefined) return new Error(`${propName} Value must be defined`)
+    if (propValue < 0) return new Error(`${propName} value must be greater than 0`)
+  }
 }
