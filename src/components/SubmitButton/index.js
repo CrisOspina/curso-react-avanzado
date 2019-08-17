f113ad01463d69a524e5899f78378ac0b4d7740a
Cristian Ospina <crisospina523@gmail.com>
2019-08-16 22:23:01 -0500

    Button reutilizable

---
+++ src/components/SubmitButton/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button } from './styles'
+import PropTypes from 'prop-types'
+
+export const SubmitButton = ({ children, onClick, disabled }) => {
+  return (
+    <Button onClick={onClick} disabled={disabled}>
+      {children}
+    </Button>
+  )
+}
+
+SubmitButton.propTypes = {
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  children: PropTypes.node.isRequired
+}
